dc62a7104fd38644f1519094b0f407fa04538985
Oscar <oscar.lang98@gmail.com>
2020-11-06 13:32:36 +0100

    fixed validation errors

--- app/models.py
+++ app/models.py
@@ -67,18 +67,30 @@ class User(UserMixin, db.Model):
         return url
 
     def follow(self, user):
+        """
+        Follow another user
+        """
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """
+        Unfollow another user
+        """
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """
+        Check if self is following another user
+        """
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        """
+        Collect all posts from all users that is followed
+        """
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)

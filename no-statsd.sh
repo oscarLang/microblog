fda63d40b6d7d6e9403a90bb898574d28549b3f4
OSCAR LANG <oscar.lang98@gmail.com>
2021-01-05 10:33:36 +0100

    Created new image without statsd

---
+++ no-statsd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+source .venv/bin/activate
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
+
+exec gunicorn --bind :5000 --access-logfile - --error-logfile - microblog:app
